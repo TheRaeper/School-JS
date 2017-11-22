7488345a621b6dde3ab754e96383ccd11ad524b4
Unknown <raedeboer@gmail.com>
2017-11-22 16:14:55 +0100

    arrays 21 en 22 done

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var weekdag = []
+weekdag.push('maandag')
+weekdag.push('dinsdag')
+weekdag.push('woensdag')
+weekdag.push('donderdag')
+weekdag.push('vrijdag')
+console.log(weekdag)
+
+document.write("<br> Opgave 21: Array aangemaakt")
+document.write("<br> Opgave 22: Elementen aangemaakt")
