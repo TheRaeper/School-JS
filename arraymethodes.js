327774c0a97667e7c4ccecc7dbb762c344988d7a
Unknown <raedeboer@gmail.com>
2017-11-24 15:24:55 +0100

    halverwege arrays methodes

--- arraymethodes.js
+++ arraymethodes.js
@@ -1,2 +1,45 @@
+//Opgave 24
 studenten = ['Zakaria', 'Albert', 'Hamsa', 'Mike', 'Pieter']
 document.write("<br> Opgave 24: Studenten: "+ studenten)
+//Opgave 25
+team = studenten
+document.write("<br> Opgave 25: Team: " + team)
+//Opgave 26
+team[0] = 'Umut';
+document.write("<br> Opgave 26: Element[0] is gewijzigd: " + studenten)
+//Opgave 27
+studenten.shift()
+document.write("<br> Opgave 27: Resultaat van shift: " + studenten)
+//Opgave 28
+studenten.unshift('Jozef','Umut')
+document.write("<br> Opgave 28: Resultaat van unshift: " + studenten)
+//Opgave 29
+studenten.pop()
+document.write("<br> Opgave 29: Resultaat van pop: " + studenten)
+//Opgave 30
+studenten.push('Pieter','David')
+document.write("<br> Opgave 30: Resultaat van push: " + studenten)
+//Opgave 31
+klas = studenten.splice(2,4);
+document.write("<br> Opgave 31: Resultaat <br>  studenten: " + studenten +"<br> klas: "+ klas)
+//Opgave 32
+studenten.sort()
+document.write("<br> Opgave 32: Resultaat van sort: " + studenten)
+//Opgave 33
+studenten.reverse()
+document.write("<br> Opgave 33: Resultaat van reverse: " + klas)
+//Opgave 34
+//bladzij 146
+//Opgave 35
+
+//Opgave 36
+
+//Opgave 37
+
+//Opgave 38
+
+//Opgave 39
+
+//Opgave 40
+
+//Opgave 41
