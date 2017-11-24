b6d038d094bd39bfad20e38f27a757e788f85c82
Unknown <raedeboer@gmail.com>
2017-11-24 17:56:26 +0100

    small changes

--- lab01.js
+++ lab01.js
@@ -1,16 +1,17 @@
-klantnaam = "Rae"
-aantal = Number(prompt("Aantal boeken te bestellen?"))
+klantnaam = (prompt("Wat is uw naam?"))
+aantal = Number(prompt("Hoeveel boeken wou u bestellen?"))
+betaling = prompt("Wat is uw gewenste manier van betalen?")
 titel = "JavaScript"
 prijs = 29.90;
 bedrag = aantal * prijs 
 btw = (bedrag * 6) / 100;
 totaal = (bedrag + btw);
 
-document.write("<br> Bedankt voor je bestelling: " 
-    + klantnaam + "<br> Boektitel is: " 
-    + titel + "<br> Aantal te bestellen: " 
-    + aantal + "<br> Prijs per boek is: " 
-    + prijs.toFixed(2) + "<br> Bedrag: "
-    + bedrag.toFixed(2) + "<br> Btw: "
-    + btw.toFixed(2) + "<br> Totaal inclusief BTW is: "
-    + totaal.toFixed(2))
+document.write("<br> Bedankt voor je bestelling: " + klantnaam)
+document.write("<br> Boektitel is: " + titel)
+document.write("<br> Aantal te bestellen: " + aantal)
+document.write("<br> Prijs per boek is: " + prijs.toFixed(2))
+document.write("<br> Bedrag: " + bedrag.toFixed(2))
+document.write("<br> Btw: " + btw.toFixed(2))
+document.write("<br> Totaal inclusief BTW is: " + totaal.toFixed(2))
+document.write("<br> Gewenste betaling: " + betaling)
