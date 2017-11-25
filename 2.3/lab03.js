9b99faf8f847e689eaa0c67f487894fe5f83af11
Unknown <raedeboer@gmail.com>
2017-11-25 10:39:02 +0100

    can't go further must ask mark

---
+++ 2.3/lab03.js
@@ -0,0 +1,5 @@
+testarray = ['een','twee','drie','vier']
+testarray.push('vijf'[5])
+document.write("<br> array test" + testarray)
+studenten = ['Zakaria', 'Albert', 'Hamsa', 'Mike', 'Pieter']
+document.write("<br> Opgave 24: Studenten: "+ studenten)
