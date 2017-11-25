295c0df14df965e7255d8913fb40a8a38074a3ac
Unknown <raedeboer@gmail.com>
2017-11-25 10:55:02 +0100

    made a start with 2.4 have same problem as i had with 2.3

---
+++ 2.4/objecten.js
@@ -0,0 +1,6 @@
+//Opgave 42
+laptops = []
+laptops ['macbook'] = {
+    'model' : 'air' , 'vooraad' : 2000, 'prijs' : 1090,
+}
+document.write("<br> Opgave 42: laptops array aangemaakt: " + laptops)
