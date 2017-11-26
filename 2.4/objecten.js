6c9bde748629a7f4628645e8dad73be4cc93aab2
Unknown <raedeboer@gmail.com>
2017-11-26 23:24:19 +0100

    opgave 43

--- 2.4/objecten.js
+++ 2.4/objecten.js
@@ -4,3 +4,9 @@ laptops ['macbook'] = {
     'model' : 'air' , 'vooraad' : 2000, 'prijs' : 1090,
 }
 document.write("<br> Opgave 42: laptops array aangemaakt: " + laptops)
+//Opgave 43
+laptops['asusbook'] = 
+{'model' : 'wind' , 'vooraad' : 1000, 'prijs' : 990,}
+laptops['dellbook'] = 
+{'model' : 'fire' , 'vooraad' : 987, 'prijs' : 890,}
+document.write("<br> Opgave 43: laptops aray uitgebreid")
