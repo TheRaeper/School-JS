d4e3a69f64de0e17575daa78c635e4d63a692322
Unknown <raedeboer@gmail.com>
2017-11-26 23:18:03 +0100

    added title and deleted stuff

--- 2.3/lab03.js
+++ 2.3/lab03.js
@@ -1,5 +1,3 @@
 testarray = ['een','twee','drie','vier']
 testarray.push('vijf'[5])
-document.write("<br> array test" + testarray)
-studenten = ['Zakaria', 'Albert', 'Hamsa', 'Mike', 'Pieter']
-document.write("<br> Opgave 24: Studenten: "+ studenten)
+]console.log("<br> array test" + testarray)
