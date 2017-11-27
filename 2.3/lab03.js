3dc4daa7336a88148d70962cdcf78eaf390cd5f1
Unknown <raedeboer@gmail.com>
2017-11-27 12:56:44 +0100

    lab03 klaar

--- 2.3/lab03.js
+++ 2.3/lab03.js
@@ -1,5 +1,33 @@
+//1
 testarray = ['een','twee','3','vier']
-testarray.push('vijf')
-document.write("<br> array test: " + testarray)
-document.write("<br> type vijfde element: " + typeof testarray[4])
-delete
+//2
+testarray.push('5')
+//3
+document.write("<br>3: " + testarray)
+//4
+document.write("<br>4: " + typeof testarray[4])
+//5
+delete testarray[4]
+//6
+document.write("<br>6: " + testarray)
+//7
+testarray.unshift('nul')
+//8
+document.write("<br>8: " + testarray)
+//9
+index = testarray.indexOf('een')
+delete index[0]
+//10
+document.write("<br>10: " + typeof index[0])
+//11
+delete testarray[0]
+//12
+document.write("<br>12: " + testarray)
+//13
+delete testarray[2]
+//14
+document.write("<br>14: " + testarray)
+//15
+delete testarray[4]
+//16
+document.write("<br>16: " + testarray)
