c56455ed235211b7a7fef7a9fe5886632f54fa14
Unknown <raedeboer@gmail.com>
2017-12-06 19:18:25 +0100

    2.6 fck this shit
    als 2de moet komen
    vandaag is: woensdag 06 december 2017
    in het nederlands but how?

--- 2.6/datemethodes.js
+++ 2.6/datemethodes.js
@@ -1,1 +1,5 @@
-maanden=new Array('januari','februari','')
+maanden=new Array('januari','februari','maart','april','mei','juni','juli','augustus','september','oktober','november','december')
+weekdag=new Array('zondag','maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag')
+vandaag=new Date()
+jaar=vandaag.getFullYear()
+document.write("<br>Vandaag is: "+vandaag+jaar);
