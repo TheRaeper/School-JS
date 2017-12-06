506217543a6315f7d4a0e15464eb76f4bb763575
Unknown <raedeboer@gmail.com>
2017-12-06 12:53:55 +0100

    finished lab05

--- 2.5/lab05.js
+++ 2.5/lab05.js
@@ -1,11 +1,8 @@
-//1
 einddatum=prompt("Wat is de einddatum JJJJ-MM-DD?")
-//2
 dealine=einddatum
-//3
-
-//4
-
-//5
-
-//6
+document.write("<br> Dit is de einddatum: "+dealine)
+vandaag =new Date(Date.now())
+datum= new Date(dealine)
+eenDag=86400000;
+resterd =((vandaag - dealine) / eenDag)
+document.write("<br> Er zijn nog "+((vandaag-datum)/eenDag).toFixed(2)+" dagen te gaan tot de deadline")
