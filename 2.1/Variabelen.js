dfb32bf2955a8e9d67c71d7e1c414212a86ab8da
Mark Kopenga <mkopenga@gmail.com>
2017-12-06 23:10:17 +0100

    improved hs 2.1

--- 2.1/Variabelen.js
+++ 2.1/Variabelen.js
@@ -1,28 +1,27 @@
 //opgave 8 vanaf hier
-voornaam  = "Carl"
-achternaam = "Petersen"
-schoolgeld = 1000;
-boekengeld = 100;
+voornaam  = `Carl`
+achternaam = `Petersen`
+schoolgeld = 1000
+boekengeld = 100
 bedrag = schoolgeld + boekengeld
-studiefinanciering = 600;
-totaal = bedrag - studiefinanciering;
-volledigenaam = voornaam + "<br>" + achternaam;
+studiefinanciering = 600
+totaal = bedrag - studiefinanciering
+volledigenaam = voornaam + `<br>` + achternaam
 document.write(
-    "<p> Opgave 8 </p>" + volledigenaam + "<br> Totaal te betalen: " + totaal
+    `<p> Opgave 8 </p>${volledigenaam}<br> Totaal te betalen: ${totaal}`
 )
-console.log("man's not hot")
+console.log(`man's hot`)
 //opgave 9 vanaf hier
-Voornaam = "Zakaria"
-Achternaam = "Bolt"
-Nederlands = 9;
-Engels = 8;
-Rekenen = 7;
-gemiddeld = (Nederlands + Engels + Rekenen) / 3;
-document.write(
-    "<p> Opgave 9 </p>" + Voornaam 
-    + " " + Achternaam 
-    + "<br> Nederlands: " + Nederlands 
-    + "<br> Engels: " + Engels 
-    + "<br> Rekenen:" + Rekenen 
-    + "<br> Gemiddelde: " + gemiddeld
-)
+Voornaam = `Zakaria`
+Achternaam = `Bolt`
+Nederlands = 9
+Engels = 8
+Rekenen = 7
+gemiddeld = (Nederlands + Engels + Rekenen) / 3
+document.write(`
+  <p> Opgave 9 </p> ${Voornaam} ${Achternaam}
+  <br> Nederlands: ${Nederlands}
+  <br> Engels: ${Engels}
+  <br> Rekenen: ${Rekenen}
+  <br> Gemiddelde: ${gemiddeld}
+`)
--- 2.1/lab01.js
+++ 2.1/lab01.js
@@ -1,14 +1,14 @@
-klantnaam = (prompt("Wat is uw naam?"))
-aantal = Number(prompt("Hoeveel boeken wou u bestellen?"))
-betaling = prompt("Wat is uw gewenste manier van betalen?")
-titel = "JavaScript"
+klantnaam = prompt(`Wat is uw naam?`)
+aantal = Number(prompt(`Hoeveel boeken wou u bestellen?`))
+betaling = prompt(`Wat is uw gewenste manier van betalen?`)
+titel = `JavaScript`
 prijs = 29.90;
 bedrag = aantal * prijs
-btw = (bedrag * 6) / 100;
-totaal = (bedrag + btw);
-document.write("<br> Bedankt voor je bestelling: " + klantnaam + "<br> Boektitel is: " + titel + "<br> Aantal te bestellen: " + aantal)
-document.write("<br> Prijs per boek is: " + prijs.toFixed(2))
-document.write("<br> Bedrag: " + bedrag.toFixed(2))
-document.write("<br> Btw: " + btw.toFixed(2))
-document.write("<br> Totaal inclusief BTW is: " + totaal.toFixed(2))
-document.write("<br> Gewenste betaling: " + betaling)
+btw = (bedrag * 6) / 100
+totaal = bedrag + btw
+document.write(`<br> Bedankt voor je bestelling: ${klantnaam}<br> Boektitel is: ${titel}<br> Aantal te bestellen: ${aantal}`)
+document.write(`<br> Prijs per boek is: ${prijs.toFixed(2)}`)
+document.write(`<br> Bedrag: ${bedrag.toFixed(2)}`)
+document.write(`<br> Btw: ${btw.toFixed(2)}`)
+document.write(`<br> Totaal inclusief BTW is: ${totaal.toFixed(2)}`)
+document.write(`<br> Gewenste betaling: ${betaling}`)
