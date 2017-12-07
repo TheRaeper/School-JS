1eddff2dfcd89ace362912302d71fe0da58a73f8
Unknown <raedeboer@gmail.com>
2017-12-07 22:53:33 +0100

    started if-else-if opdracht 55

--- 2.7/if-opdracht.js
+++ 2.7/if-opdracht.js
@@ -1,9 +1,28 @@
 let wl = (d) => {document.write(d);document.write('<br>')}
 
 let doorgaan = confirm(`Opgave 52: Doorgaan?`)
-
+//opgave 52
 if (doorgaan == true) {
   wl(`Welkom bij de if-opdracht`)
-} else {
-  wl(`NIET welkom nij de if-opdracht`)
+}
+//Opgave 53
+else {
+  wl(`tot ziens`)
+}
+//Opgave 54
+if (doorgaan == true) {
+  leeftijd = parseInt(prompt(`Wat is je leeftijd?`))
+  if (leeftijd < 17) {
+    leerplichtig = true
+  }
+}
+else {
+  leerplichtig = false
+}
+gediplomeerd = confirm(`Heb je je diploma behaald?`)
+if (leerplichtig && !gediplomeerd) {
+  alert(`Doorstuderen!`)
+}
+if (gediplomeerd || !leerplichtig) {
+  alert(`Je bent gediplomeerd Of niet leerplichtig Of allebei`)
 }
