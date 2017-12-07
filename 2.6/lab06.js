adfb4b25d34d90f2e0be80cdeaed5e9bbe8fcb42
Unknown <raedeboer@gmail.com>
2017-12-07 17:39:21 +0100

    small changes lab06
    es6 is awasome and a pain in the arsehole

--- 2.6/lab06.js
+++ 2.6/lab06.js
@@ -1,6 +1,15 @@
 let klantnaam = prompt(`Wat is uw naam?`)
-let aantaal = Number(promt(`Hoeveel boeken wilt u bestellen?`))
+let aantal = Number(prompt(`Hoeveel boeken wilt u bestellen?`))
 let vandaag = new Date()
+Date.prototype.addDays = function(days) {
+  dat = new Date(this.valueOf())
+  dat.setDate(dat.getDate() + days)
+  return dat;
+}
+dat = new Date()
+dat2 = dat.addDays(5)
 document.write(`<br>Lab06`)
 document.write(`<br>Factuur`)
-document.write(`<br>Factuurdatum: <b>${ vandaag.getDay()] }</b>, - <b>${ vandaag.getMonth()] }</b>, - <b>${ vandaag.getYear()] }</b>`)
+document.write(`<br>Factuurdatum: <b>${vandaag.getDay() }</b> - <b>${vandaag.getMonth() }</b> - <b>${vandaag.getYear() }</b>`)
+document.write(`<br>Betaaldatum: ${dat2}`)
+document.write(`<br>Bedankt voor je bestelling: ${klantnaam}`)
