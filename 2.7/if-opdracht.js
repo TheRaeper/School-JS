b735368fb42f8418124d0607572b7330b66f26af
Unknown <raedeboer@gmail.com>
2017-12-07 20:33:54 +0100

    small changes updated lab06 works again

---
+++ 2.7/if-opdracht.js
@@ -0,0 +1,5 @@
+//Opgave 52
+doorgaan = confirm(`Opgave 52: Doorgaan?`)
+if (doorgaan) == true {
+  alert(`Welkom nij de if-opdracht`)
+}
--- everything/everything.js
+++ everything/everything.js
@@ -198,28 +198,26 @@
         let vandaag = new Date()
     document.write(`<br>Vandaag is Dag: <b>${ weekdag[vandaag.getDay()] }</b>, maand: <b>${ maanden[vandaag.getMonth()] }</b>, Jaar: <b>${ vandaag.getFullYear() }</b>`);
 //Lab06
-      let klantnaam = prompt(`Wat is uw naam?`)
-      let aantal = Number(prompt(`Hoeveel boeken wilt u bestellen?`))
-      let vandaag = new Date()
-      let boek = (`JavaSctipt`)
-      let prijsboek = 29.90
-      let zonderbtw = aantal*prijsboek
-      let btw = (zonderbtw*6)/100
-      let metbtw = zonderbtw+btw
+        let klantnaam = prompt(`Wat is uw naam?`)
+        let aantal = Number(prompt(`Hoeveel boeken wilt u bestellen?`))
+        let vandaag = new Date()
+        let titel = (`JavaSctipt`)
+        let prijs = 29.90
+        let bedrag = aantal * prijs
+        let btw = (bedrag * 6) / 100
+        let totaal = bedrag+btw
     Date.prototype.addDays = function(days) {
-      dat = new Date(this.valueOf())
-      dat.setDate(dat.getDate() + days)
-      return dat;
-    }
-      let dat = new Date()
+        dat = new Date(this.valueOf())
+        dat.setDate(dat.getDate() + days)
+        return dat;
+}
+        dat = new Date()
     document.write(`<br>Lab06`)
     document.write(`<br>Factuur`)
     document.write(`<br>Factuurdatum: ${vandaag}`)
     document.write(`<br>Betaaldatum: ${dat.addDays(5)}`)
-    document.write(`<br>Bedankt voor je bestelling: ${klantnaam}`)
-    document.write(`<br>Boektitel: ${boek}`)
-    document.write(`<br>Aantal te bestellen: ${aantal}`)
-    document.write(`<br>Prijs per boek is: €${prijsboek.toFixed(2)}`)
-    document.write(`<br>Bedrag zonder BTW: €${zonderbtw.toFixed(2)}`)
-    document.write(`<br>De BTW bedraagt: €${metbtw.toFixed(2)}`)
-    document.write(`<br>Totaalprijs inclusief btw: €${metbtw.toFixed(2)}`)
+    document.write(`<br> Bedankt voor je bestelling: ${klantnaam}<br> Boektitel is: ${titel}<br> Aantal te bestellen: ${aantal}`)
+    document.write(`<br> Prijs per boek is: ${prijs.toFixed(2)}`)
+    document.write(`<br> Bedrag: ${bedrag.toFixed(2)}`)
+    document.write(`<br> Btw: ${btw.toFixed(2)}`)
+    document.write(`<br> Totaal inclusief BTW is: ${totaal.toFixed(2)}`)
