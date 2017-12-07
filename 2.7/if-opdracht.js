0f329f838430a810c90aacad59ef36d0b7d0cae5
Mark Kopenga <mkopenga@gmail.com>
2017-12-07 21:13:21 +0100

    fixed the "if opdracht" and added some tips

--- 2.7/if-opdracht.js
+++ 2.7/if-opdracht.js
@@ -1,5 +1,21 @@
 //Opgave 52
-doorgaan = confirm(`Opgave 52: Doorgaan?`)
-if (doorgaan) == true {
-  alert(`Welkom nij de if-opdracht`)
+let wl = (d) => {document.write(d);document.write('<br>')}
+
+let doorgaan = confirm(`Opgave 52: Doorgaan?`)
+
+// standaard marnier
+if (doorgaan == true) {
+  wl(`Welkom nij de if-opdracht`)
+} else {
+  wl(`NIET welkom nij de if-opdracht`)
 }
+
+// als je alleen wil weten of een variable "true" is kan je dat ook op deze marnier doen
+if (doorgaan) {
+  wl(`Welkom nij de if-opdracht`)
+} else {
+  wl(`NIET welkom nij de if-opdracht`)
+}
+
+// en nu in 1 lijn
+(doorgaan) ? wl(`Welkom nij de if-opdracht`) : wl(`NIET welkom nij de if-opdracht`)
