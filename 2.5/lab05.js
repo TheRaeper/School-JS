65ec4d95c8e17acb43e3689dd23eaca1a8a761b8
Unknown <raedeboer@gmail.com>
2017-12-08 19:14:23 +0100

    small change 2.5

--- 2.5/lab05.js
+++ 2.5/lab05.js
@@ -1,8 +1,10 @@
-einddatum=prompt("Wat is de einddatum JJJJ-MM-DD?")
+einddatum=prompt(`Wat is de einddatum JJJJ-MM-DD?`)
 dealine=einddatum
-document.write("<br> Dit is de einddatum: "+dealine)
+document.write(`<br>Dit is de einddatum: ${deadline}`)
 vandaag =new Date(Date.now())
 datum= new Date(dealine)
 eenDag=86400000;
 resterd =((vandaag - dealine) / eenDag)
-document.write("<br> Er zijn nog "+((vandaag-datum)/eenDag).toFixed(2)+" dagen te gaan tot de deadline")
+document.write(`<br>Er zijn nog
+  ${(vandaag-datum)/eenDag.toFixed(2)}
+  dagen te gaan tot de deadline`)
