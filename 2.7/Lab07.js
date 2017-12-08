79f59ec63fd9d79c5d79fa03d883f716539f518b
Unknown <raedeboer@gmail.com>
2017-12-08 14:28:30 +0100

    Auto stash before merge of "master" and "origin/master"

--- 2.7/Lab07.js
+++ 2.7/Lab07.js
@@ -1,3 +1,44 @@
 let wl = (d) => {document.write(d);document.write('<br>')}
 //Lab07
-penisprikkel = `${`${`${`${`${`${`${`${`${`${`${`${`${`${`${`${`${`${`${`${`${`${`${'why not'}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`}`
+let klantnaam = prompt(`Wat is uw naam?`)
+let aantal = Number(prompt(`Hoeveel boeken wilt u bestellen?`))
+let woonplaats = prompt(`Wat is uw woonplaats?`)
+let postcode = prompt(`Wat is uw postcode?`)
+
+let vandaag = new Date()
+let titel = (`JavaSctipt`)
+let prijs = 29.90
+
+let bedrag = aantal * prijs
+let btw = (bedrag * 6) / 100
+let totaal = bedrag+btw
+let bezorgkosten = 15
+
+var woonplaats1 = (woonplaats === `amsterdam` ? true : false)
+var postcode1 = (postcode >= `1000AA` <= `2000BB` ? true : false)
+if (woonplaats1 && postcode1){
+  bezorgkosten = 0
+  alert(`U betaalt geen bezorgkosten.`)
+}
+else {
+  bezorgkosten = 15
+}
+Date.prototype.addDays = function(days) {
+  dat = new Date(this.valueOf())
+  dat.setDate(dat.getDate() + days)
+  return dat;
+}
+dat = new Date()
+
+document.write(`<br>Lab06
+<br>Factuur
+<br>Factuurdatum: ${vandaag}
+<br>Betaaldatum: ${dat.addDays(5)}
+<br>Bedankt voor je bestelling: ${klantnaam}
+<br>Boektitel is: ${titel}
+<br>Aantal te bestellen: ${aantal}
+<br>Prijs per boek is: ${prijs.toFixed(2)}
+<br>Bedrag: ${bedrag.toFixed(2)}
+<br>Btw: ${btw.toFixed(2)}
+<br>Totaal inclusief BTW is: ${totaal.toFixed(2)}`)
+document.write(`<br>Bezorgkosten: ${bezorgkosten}`)
