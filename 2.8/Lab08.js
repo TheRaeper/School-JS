0e163230bddd00b52b5fce7e60e8175e3c9671b5
Mark Kopenga <mkopenga@gmail.com>
2017-12-08 20:10:40 +0100

    fixed the date

--- 2.8/Lab08.js
+++ 2.8/Lab08.js
@@ -1,5 +1,6 @@
 // opdracht hier
 let wl = (d) => {document.write(d);document.write('<br>')}
 
-dat = new Date(Date.now())
-document.write(`Vandaag is: ${dat}`)
+let dat = new Date().now()
+
+wl(`Vandaag is: ${dat}`)
