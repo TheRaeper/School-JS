5546c362f1693ee71ecb8d7523c01d6b6d918e2c
Unknown <raedeboer@gmail.com>
2017-12-10 23:05:07 +0100

    fck 2.9

--- 2.9/switch-opdracht.js
+++ 2.9/switch-opdracht.js
@@ -4,23 +4,29 @@ let datum = new Date()
 let vandaag = datum.getDay()
 let uur = datum.getHours()
 
-wl(`Het is vandaag: ${datum.toDateString()}`)
+wl(`Het is vandaag: ${datum}`)
 
 switch (vandaag){
   case 1:
-    wl(`maandag is mijn eerste stagedag.`)
+    document.write(`maandag is mijn eerste stagedag.`)
     break
   case 2:
-    wl(`dinsdag is mijn eerste stagedag.`)
+    document.write(`dinsdag is mijn tweede stagedag.`)
     break
   case 3:
-    wl(`woensdag is mijn eerste stagedag.`)
+    document.write(`woensdag is mijn JavaScriptdag.`)
     break
   case 4:
-    wl(`donderdag is mijn eerste stagedag.`)
+    document.write(`donderdag is mijn werkdag.`)
     break
   case 5:
-    wl(`vrijdag is mijn eerste stagedag.`)
+    document.write(`vrijdag is mijn roostervrije dag.`)
+    break
+  case 5:
+    document.write(`zaterdag is mijn vrije dag.`)
+    break
+  case 6:
+    document.write(`zondag is mijn vrije dag`)
     break
 }
 // hieronder hoe het moet volgens het boek wtf heb ik fout?
