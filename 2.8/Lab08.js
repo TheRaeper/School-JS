fb30020c4130647928746a3bb40771068783c8f1
Unknown <raedeboer@gmail.com>
2017-12-10 19:21:22 +0100

    almost done 2.8
    moet nog ff 
    if ( datum van vandaag hetzelfe is als de betaaldatum) { 
    alert(`melden incasso bureau`
    }
    else {
    wl(`u heeft nog (dagen) om te betalen`)
    }

--- 2.8/Lab08.js
+++ 2.8/Lab08.js
@@ -1,7 +1,23 @@
-// opdracht hier
+//Lab08
 let wl = (d) => {document.write(d);document.write('<br>')}
 
-let dat = new Date().now()
+Date.prototype.addDays = function(days) {
+  dat = new Date(this.valueOf())
+  dat.setDate(dat.getDate() + days)
+  return dat;
+}
+let dat = new Date()
 
 wl(`Vandaag is: ${dat}`)
-//added lights in my christmas tree
+let factuurdatum = dat.addDays(-16)
+wl(`Factuurdatum is: ${factuurdatum}`)
+let betaaldatum = factuurdatum.addDays(30)
+wl(`Betaaldatum is: ${betaaldatum}`)
+
+let vandaag = Date.now()
+if (vandaag == betaaldatum) {
+  alert(`Meld incassobureau!`)
+}
+else {
+  vandaag - betaaldatum
+}
