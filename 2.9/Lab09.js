df6f3c47ca807740fb88708e527f4cf85c0c6e9f
Unknown <raedeboer@gmail.com>
2017-12-12 16:25:44 +0100

    zucht lukt nieet aAARHHHCg

--- 2.9/Lab09.js
+++ 2.9/Lab09.js
@@ -3,21 +3,12 @@ let wl = (d) => {document.write(d);document.write(`<br>`)}
 let rol = prompt(`Wat is je rol?`)
 let inlogcode = prompt(`Wat is je inlogcode?`)
 
-//dit in een switch verwerken
-if (rol == `admin` && inlogcode == `admin`) {
-  wl(`Welkom admin`)
-}
-else {
-  wl(`Code ongeldig`)
-}
-
-if (rol == `student` && inlogcode == `student`) {
-  wl(`Welkom student`)
-}
-else {
-  wl(`Code ongeldig`)
-}
-
-if (rol == ``) {
-  wl(`Code ongeldig`)
+switch (rol && inlogcode){
+  case `admin`:
+    wl(`Welkom admin`)
+    break
+  case `student`:
+    wl(`Welkom student`)
+  default:
+    wl(`Code ongeldig`)
 }
