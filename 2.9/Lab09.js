4c43597de58ae9be9339b24a72bf9aad5d750458
Unknown <raedeboer@gmail.com>
2017-12-15 15:15:51 +0100

    2.9 done

--- 2.9/Lab09.js
+++ 2.9/Lab09.js
@@ -3,12 +3,13 @@ let wl = (d) => {document.write(d);document.write(`<br>`)}
 let rol = prompt(`Wat is je rol?`)
 let inlogcode = prompt(`Wat is je inlogcode?`)
 
-switch (rol && inlogcode){
+switch (rol && inlogcode) {
   case `admin`:
     wl(`Welkom admin`)
-    break
+    break;
   case `student`:
     wl(`Welkom student`)
+    break;
   default:
-    wl(`Code ongeldig`)
+    wl(`code ongeldig`)
 }
