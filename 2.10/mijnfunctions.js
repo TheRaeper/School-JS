62b5a2cc3c4cdb6516d35218bacefffa793653d5
Mark Kopenga <mkopenga@gmail.com>
2018-01-07 11:33:42 +0100

    added some commands to inform someone who is reading this code

--- 2.10/mijnfunctions.js
+++ 2.10/mijnfunctions.js
@@ -2,13 +2,15 @@ wl = (d) => {document.write(d);document.write(`<br>`)}
 
 //opgave 59
 verdubbel = (inputgetal) =>
-  inputgetal * 2
+  (typeof inputgetal == 'number') ? // check if "inputgetal" is a number,
+    inputgetal * 2 : // if so return that times 2
+    0  // if "inputgetal" is not a number return 0 (I think it's better to return a 0 because if you want to use the value later you are shure its a number)
 
 //opgave 60
 afmelden = () =>
-  confirm(`Wilt u zich afmelden?`) ?
-    alert(`U wordt afgemeld!!!`) :
-    alert('U bent niet afgemeld.')
+  confirm(`Wilt u zich afmelden?`) ? // if the ouput of the funciton: confirm is true
+    alert(`U wordt afgemeld!!!`) : // alert the user: `U wordt afgemeld!!!`
+    alert('U bent niet afgemeld.') // if the output of the first funciton is false alert: 'U bent niet afgemeld.
 
 //opgave 61
 let mijngetal = prompt(`Typ getal in`)
@@ -19,10 +21,10 @@ afmelden()
 
 //opgave 62
 let begroeten = Date.now()
-let uur = getHours()
+let uur = getHours() // what is getHours ???
 if (uur >= getHours(0) <= getHours(11)){
   wl(`Goedemorgen`)
-} else if (uur >= getHours(12) <= getHours(17)){
+} if else (uur >= getHours(12) <= getHours(17)){ // there is something called a if else you know :D
   wl(`Goedemiddag`)
 } else {
   wl('Goedenavond')
